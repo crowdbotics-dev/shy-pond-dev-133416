788a188120ad911bb376d7cf78787c7f6d97f3a2
Crowdbotics <engineering@crowdbotics.com>
2024-02-13 23:17:50 +0000

    Initial commit

---
+++ backend/shy_pond_dev_133416/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for shy_pond_dev_133416 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "shy_pond_dev_133416.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shy-pond-dev-133416",
+  url: "https://shy-pond-dev-133416.botics.co",
+  api: "https://shy-pond-dev-133416.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
